77edc350e96cc6714ebf546a01a354c2977cd65c
donskilful <donskilful@gmail.com>
2021-12-19 20:59:42 +0100

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -14,6 +15,7 @@ mongoose.connect( process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => console.log("Database connected")).catch(err=>console.log(err));
 
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
